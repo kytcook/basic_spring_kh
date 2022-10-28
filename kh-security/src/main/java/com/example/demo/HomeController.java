0efead99ca421234bc6796be19568d85518286ca
kytcook <kytcook@naver.com>
2022-10-28 09:53:59 +0900

    SecuritySetting_221028

---
+++ kh-security/src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,43 @@
+package com.example.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+	Logger logger = LoggerFactory.getLogger(HomeController.class);
+	@GetMapping({"","/"})
+	public String home() {
+		logger.info("home호출");
+		return "redirect:home.jsp";
+	}
+	
+	@GetMapping({"/user"})
+	public @ResponseBody String user() {
+		return "유저페이지 입니다.";
+	}
+	
+	@GetMapping({"/admin"})
+	public @ResponseBody String admin() {
+		return "관리자페이지 입니다.";
+	}
+	
+	@GetMapping({"/loginForm"})
+	public String loginForm() {
+		return "redirect:loginForm.jsp";
+	}
+	
+	@GetMapping({"/memberForm"})
+	public String memberForm() {
+		return "redirect:memberForm.jsp";
+	}
+	
+	/* 나중에 직접 만들어 보아요~ */
+	@GetMapping({"/manager"})
+	public @ResponseBody String manger() {
+		return "매니저페이지 입니다.";
+	}
+}
---
+++ kh-security/src/main/java/com/example/demo/KHSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KHSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KHSecurityApplication.class, args);
+	}
+
+}
